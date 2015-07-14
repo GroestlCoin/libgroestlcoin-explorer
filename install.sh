libbitcoin/libbitcoin-explorer

Regenerated .travis.yml

install.sh
@@ -197,6 +197,7 @@ BOOST_OPTIONS_GCC=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -210,6 +211,7 @@ BOOST_OPTIONS_CLANG=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -249,6 +251,20 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
+"--without-examples "\
+"${with_boost} "\
+"${with_pkgconfigdir} "
+
+# Define protobuf options.
+#------------------------------------------------------------------------------
+PROTOBUF_OPTIONS=\
+"--enable-silent-rules "
+
+# Define bitcoin-protocol options.
+#------------------------------------------------------------------------------
+BITCOIN_PROTOCOL_OPTIONS=\
+"--without-tests "\
+"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
@@ -583,6 +599,8 @@ build_all()
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github pmienk libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin protobuf 2.6.0 $SEQUENTIAL "$@" $PROTOBUF_OPTIONS
+    build_from_github pmienk libbitcoin-protocol master $PARALLEL "$@" $BITCOIN_PROTOCOL_OPTIONS
     build_from_github pmienk libbitcoin-client master $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
     build_from_travis pmienk libbitcoin-explorer master $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }

GroestlCoin/libgroestlcoin-explorer

install.sh
@@ -216,6 +216,7 @@ BOOST_OPTIONS_GCC=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -229,6 +230,7 @@ BOOST_OPTIONS_CLANG=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -268,6 +270,20 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
+"--without-examples "\
+"${with_boost} "\
+"${with_pkgconfigdir} "
+
+# Define protobuf options.
+#------------------------------------------------------------------------------
+PROTOBUF_OPTIONS=\
+"--enable-silent-rules "
+
+# Define bitcoin-protocol options.
+#------------------------------------------------------------------------------
+BITCOIN_PROTOCOL_OPTIONS=\
+"--without-tests "\
+"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
@@ -602,6 +618,8 @@ build_all()
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github pmienk libbitcoin refactor_types $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin protobuf 2.6.0 $SEQUENTIAL "$@" $PROTOBUF_OPTIONS
+    build_from_github pmienk libbitcoin-protocol refactor_types $PARALLEL "$@" $BITCOIN_PROTOCOL_OPTIONS
     build_from_github pmienk libbitcoin-client refactor_types $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
     build_from_travis pmienk libbitcoin-explorer refactor_types $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }