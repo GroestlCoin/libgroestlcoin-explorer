libbitcoin/libbitcoin-explorer

Alter to support libbitcoin changes.

install.sh
@@ -249,7 +249,6 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
-"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
@@ -583,9 +582,9 @@ build_all()
     build_from_github zeromq czmq master $PARALLEL "$@" $CZMQ_OPTIONS
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
-    build_from_github libbitcoin libbitcoin-client master $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
-    build_from_travis libbitcoin libbitcoin-explorer master $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
+    build_from_github pmienk libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github pmienk libbitcoin-client master $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
+    build_from_travis pmienk libbitcoin-explorer master $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }
 
 

GroestlCoin/libgroestlcoin-explorer

install.sh
@@ -268,7 +268,6 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
-"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
@@ -602,9 +601,9 @@ build_all()
     build_from_github zeromq czmq master $PARALLEL "$@" $CZMQ_OPTIONS
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_github libbitcoin libbitcoin version2 $PARALLEL "$@" $BITCOIN_OPTIONS
-    build_from_github libbitcoin libbitcoin-client version2 $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
-    build_from_travis libbitcoin libbitcoin-explorer version2 $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
+    build_from_github pmienk libbitcoin refactor_types $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github pmienk libbitcoin-client refactor_types $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
+    build_from_travis pmienk libbitcoin-explorer refactor_types $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }
 
 