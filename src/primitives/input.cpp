libbitcoin/libbitcoin-explorer

Adapt to libbitcoin changes.

src/primitives/input.cpp
@@ -43,9 +43,10 @@ static bool decode_input(tx_input_type& input, const std::string& tuple)
     if (tokens.size() != 2 && tokens.size() != 3)
         return false;
 
-    input.script = chain::script();
-    input.sequence = max_sequence;
-    input.previous_output = point(tokens[0] + ":" + tokens[1]);
+    input.script(chain::script());
+    input.sequence(max_sequence);
+    input.previous_output(point(tokens[0] + ":" + tokens[1]));
+
     if (tokens.size() == 3)
         deserialize(input.sequence, tokens[2], true);
 
@@ -56,8 +57,8 @@ static bool decode_input(tx_input_type& input, const std::string& tuple)
 static std::string encode_input(const tx_input_type& input)
 {
     std::stringstream result;
-    result << point(input.previous_output) << BX_TX_POINT_DELIMITER << 
-        input.sequence;
+    result << point(input.previous_output()) << BX_TX_POINT_DELIMITER <<
+        input.sequence();
     return result.str();
 }
 
@@ -83,9 +84,9 @@ input::input(const input& other)
 
 input::input(const chain::input_point& value)
 {
-    value_.previous_output = value;
-    value_.sequence = max_sequence;
-    value_.script = chain::script();
+    value_.previous_output(value);
+    value_.sequence(max_sequence);
+    value_.script(chain::script());
 }
 
 input::operator const tx_input_type&() const

GroestlCoin/libgroestlcoin-explorer

src/primitives/input.cpp
@@ -43,9 +43,10 @@ static bool decode_input(tx_input_type& input, const std::string& tuple)
     if (tokens.size() != 2 && tokens.size() != 3)
         return false;
 
-    input.script = chain::script();
-    input.sequence = max_input_sequence;
-    input.previous_output = point(tokens[0] + ":" + tokens[1]);
+    input.script(chain::script());
+    input.sequence(max_input_sequence);
+    input.previous_output(point(tokens[0] + ":" + tokens[1]));
+
     if (tokens.size() == 3)
         deserialize(input.sequence, tokens[2], true);
 
@@ -56,8 +57,8 @@ static bool decode_input(tx_input_type& input, const std::string& tuple)
 static std::string encode_input(const tx_input_type& input)
 {
     std::stringstream result;
-    result << point(input.previous_output) << BX_TX_POINT_DELIMITER << 
-        input.sequence;
+    result << point(input.previous_output()) << BX_TX_POINT_DELIMITER <<
+        input.sequence();
     return result.str();
 }
 
@@ -83,9 +84,9 @@ input::input(const input& other)
 
 input::input(const chain::input_point& value)
 {
-    value_.previous_output = value;
-    value_.sequence = max_input_sequence;
-    value_.script = chain::script();
+    value_.previous_output(value);
+    value_.sequence(max_input_sequence);
+    value_.script(chain::script());
 }
 
 input::operator const tx_input_type&() const